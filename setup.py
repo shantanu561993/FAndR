167ac04a04d840398cca36cd71f1a5b1e9eb8024
shantanu561993 <shantanu561993@gmail.com>
2015-07-12 11:00:07 +0530

    changes to setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 setup(
-    name='FAR',
+    name='FindAndReplace',
     version='0.1.0',
     description="This project will find text in files and replace the text",
     long_description=readme + '\n\n' + history,
@@ -23,14 +23,14 @@ setup(
     author_email='shantanu561993@gmail.com',
     url='https://github.com/shantanu561993/FAR',
     packages=[
-        'FAR',
+        'FindAndReplace',
     ],
     package_dir={'FindAndReplace':
                  'FindAndReplace'},
     include_package_data=True,
     license="GPLv3+",
     zip_safe=False,
-    keywords='FAR',
+    keywords='Find And Replace',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
